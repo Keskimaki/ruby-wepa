f7b2b683c469b70164dc674101e04d15b3bbf514
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-08-13 17:52:52 +0300

    Make deleting ratings possible

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   resources :beers
   resources :breweries
+  resources :ratings, only: [:index, :new, :create, :destroy]
 
   root "breweries#index"
-
-  get "ratings", to: "ratings#index"
-  get "ratings/new", to: "ratings#new"
-
-  post "ratings", to: "ratings#create"
 end
