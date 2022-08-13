443cd83940ee2558f7b2ef29e279d5ca2c232ec0
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-08-13 16:23:29 +0300

    Create method for beer's average rating and show value in beer view

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,15 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    return 0 if ratings.empty?
+
+    score = 0
+    ratings.each do |rating|
+      score += rating.score
+    end
+
+    return score / ratings.count
+  end
 end
