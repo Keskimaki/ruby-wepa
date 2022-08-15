3704f44bd93f389ccf2bb2bac8a278a44f9355f3
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-08-15 10:24:53 +0300

    Update routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   resources :ratings, only: [:index, :new, :create, :destroy]
   resource :session, only: [:new, :create, :destroy]
 
-  get 'signup', to: 'users#new'
+  get "signup", to: "users#new"
+  get "signin", to: "sessions#new"
+  delete "signout", to: "sessions#destroy"
 
   root "breweries#index"
 end
