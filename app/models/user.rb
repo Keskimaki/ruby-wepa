faaec3ef7585971b1d5ee425c3ced12ced8254cc
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-08-25 16:37:58 +0300

    Add beer and user many to many relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   include RatingAverage
 
+  has_many :ratings
+  has_many :beers, through: :ratings
+
   validates :username, uniqueness: true,
     length: { minimum: 3, maximum: 30 }
-
-  has_many :ratings
 end
