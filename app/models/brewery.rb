f08f7380cbe4482e057a46db8a0f0bd204d86180
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-08-25 16:29:11 +0300

    Brewery founding year has to be less than or equal to current year

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -6,7 +6,7 @@ class Brewery < ApplicationRecord
 
   validates :name, presence: true
   validates :year, numericality: { greater_than_or_equal_to: 1040,
-    less_than_or_equal_to: 2022,
+    less_than_or_equal_to: ->(_obj) { Date.current.year },
     only_integer: true }
 
   def print_report
