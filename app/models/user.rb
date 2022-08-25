6722053e36390edfe5d0ed6298ea7cd9a06d28e3
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-08-25 17:37:11 +0300

    Delete user's ratings when user deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
 
   validates :username, uniqueness: true,
