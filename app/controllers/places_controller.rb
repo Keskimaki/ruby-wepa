b94b6fa9ac057e347f9c4cf9ef88adb0283cae0f
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-09-12 12:08:41 +0300

    Encode city name in url, add link to place search to navbar

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,7 +5,7 @@ class PlacesController < ApplicationController
   def search
     api_key = ""
     url = "http://beermapping.com/webservice/loccity/#{api_key}/"
-    response = HTTParty.get "#{url}#{params[:city]}"
+    response = HTTParty.get "#{url}#{ERB::Util.url_encode(params[:city])}"
     places_from_api = response.parsed_response["bmp_locations"]["location"]
 
     if places_from_api.is_a?(Hash) && places_from_api['id'].nil?
