e6864bea4f1d90d03ea92253a79b7f5d4fe28cc1
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-09-12 12:15:59 +0300

    Refactor place search logic

---
+++ lib/beermapping_api.rb
@@ -0,0 +1,19 @@
+class BeermappingApi
+  def self.places_in(city)
+    url = "http://beermapping.com/webservice/loccity/#{key}/"
+
+    response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+    places = response.parsed_response["bmp_locations"]["location"]
+
+    return [] if places.is_a?(Hash) and places['id'].nil?
+
+    places = [places] if places.is_a?(Hash)
+    places.map do | place |
+      Place.new(place)
+    end
+  end
+
+  def self.key
+    "e747ef47e7b4e7e8d8f0e8bd45765852"
+  end
+end
