b5e0762ee2662b9cb93b01dd0eb96089111cc44d
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-09-12 11:51:23 +0300

    Create placeholders of place search feature

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ Rails.application.routes.draw do
   get "signin", to: "sessions#new"
   delete "signout", to: "sessions#destroy"
 
+  get 'places', to: 'places#index'
+  post 'places', to: 'places#search'
+
   root "breweries#index"
 end
